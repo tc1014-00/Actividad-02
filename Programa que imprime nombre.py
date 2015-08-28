2e2f6c8c4257f15d6e14724b806617b6e79592a5
A01377647 <hectak97.HT@gmail.com>
2015-08-27 22:44:14 -0500

    Tarea 2
    Anexo a continuación los programas escritos en Calico con sus
    respectivas soluciones.
    
    Héctor Manuel Takami Flores
    A01377647

---
+++ Programa que imprime nombre.py
@@ -0,0 +1,23 @@
+#encoding: UTF-8
+#Autor: Hector Manuel Takami Flores
+#Programa que imprime el nombre
+
+nombre = str(input("Ingresa tu Nombre"))
+apellido1= str(input("Ingrese Apellido Paterno"))
+apellido2= str(input("Ingrese Apellido Materno"))
+
+matricula = str(input("Ingrese matricula"))
+
+carrera= str(input("¿Cual es tu carrera?"))
+
+color = str(input("¿Cual es tu color favorito?"))
+hobby = str(input("Cuentame... ¿Que deporte/instrumento musical precticas?"))
+cumpleaños = str(input("¿Cuando es tu cumpleaños?"))
+
+print("Eres:",nombre,apellido1,apellido2)
+print ("Tu matricula es:",matricula)
+print ("Estas en la carrera de:",carrera)
+print("Te gusta el color",color)
+print("Sueles practicar",hobby)
+print("Y naciste el",cumpleaños)
+print("AL PARECER ERES UNA PERSONA INTERESANTE")
